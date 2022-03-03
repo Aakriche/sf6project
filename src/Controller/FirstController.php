8dc4ecf15f2a2795b541baaf3b12362116118d42
aakriche <amaury@akriche.com>
2022-03-03 14:44:11 +0100

    tests de controllers

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -11,8 +11,33 @@ class FirstController extends AbstractController
     #[Route('/first', name: 'app_first')]
     public function index(): Response
     {
+
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
     }
+
+    #[Route('/sayHello', name: 'say_hello')]
+    public function sayHello(): Response
+    {
+        $rand = rand(0, 4);
+        echo $rand;
+        if ($rand == 2) {
+            return $this->redirectToRoute('app_first');
+        }
+            return $this->forward('App\\Controller\FirstController::index');
+
+    }
+
+    #[Route('/sayHello/{name}/{firstname}', name: 'say_hello')]
+    public function sayHelloToSomeone($name, $firstname): Response
+    {
+
+        return $this->render('first/index.html.twig', [
+            'name' => $name,
+            'firstname' => $firstname
+        ]);
+
+    }
+
 }
